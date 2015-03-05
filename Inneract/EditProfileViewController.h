fe3f6f7700425923019d449318370142f880511d
Emmanuel Texier <etexier@yahoo.com>
2015-03-05 10:47:36 -0800

    Added EditProfileViewController

---
+++ Inneract/EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  Inneract
+//
+//  Created by Emmanuel Texier on 3/5/15.
+//  Copyright (c) 2015 Emmanuel Texier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfileViewController : UIViewController
+
+@end
