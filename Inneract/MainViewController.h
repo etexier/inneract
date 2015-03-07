4a5a3e47ca3c2a7e8a026aa7b8dcd270c40a073d
Jim Liu <jim.liu@magnet.com>
2015-03-07 13:10:12 -0800

    added main view controller

---
+++ Inneract/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Inneract
+//
+//  Created by Jim Liu on 3/7/15.
+//  Copyright (c) 2015 Emmanuel Texier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
