16c3b5e4e2dc6254f79a4f063f0dc636f688f5b9
Emmanuel Texier <etexier@yahoo.com>
2015-03-08 16:34:59 -0700

    adding colors according to IP_app_Color.pdf

---
+++ Inneract/Utils/IPColors.h
@@ -0,0 +1,34 @@
+//
+//  IPColors.h
+//  Inneract
+//
+//  Created by Emmanuel Texier on 3/8/15.
+//  Copyright (c) 2015 Emmanuel Texier. All rights reserved.
+//
+
+#ifndef Inneract_IPColors_h
+#define Inneract_IPColors_h
+
+// See IP_app_Color.pdf
+
+// primary colors
+#define ipPrimaryOrange [UIColor colorWithRed:1 green:0.702 blue:0.008 alpha:1] /*#ffb302*/
+#define ipPrimaryMidnightBlue [UIColor colorWithRed:0.22 green:0.243 blue:0.478 alpha:1] /*#383e7a*/
+#define ipPrimaryBlack [UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:1] /*#333333*/
+#define ipPrimaryLightGrey [UIColor colorWithRed:0.976 green:0.976 blue:0.976 alpha:1] /*#f9f9f9*/
+
+
+// secondary accent colors
+#define ipSecondaryGrey [UIColor colorWithRed:0.612 green:0.62 blue:0.737 alpha:1] /*#9c9ebc*/
+#define ipSecondaryPurple [UIColor colorWithRed:0.373 green:0.231 blue:0.447 alpha:1] /*#5f3b72*/
+#define ipSecondaryCyan [UIColor colorWithRed:0.529 green:0.886 blue:0.82 alpha:1] /*#87e2d1*/
+#define ipSecondaryBlue [UIColor colorWithRed:0.227 green:0.463 blue:0.71 alpha:1] /*#3a76b5*/
+#define ipSecondaryPink [UIColor colorWithRed:1 green:0.573 blue:0.675 alpha:1] /*#ff92ac*/
+#define ipSecondaryLightGrey[UIColor colorWithRed:0.922 green:0.925 blue:0.945 alpha:1] /*#ebecf1*/
+
+
+
+
+
+
+#endif
