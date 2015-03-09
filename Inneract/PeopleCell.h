678b61a4da447c006dcf21d6eab78db965a804fe
Jim Liu <jim.liu@magnet.com>
2015-03-08 23:20:43 -0700

    add people cell

---
+++ Inneract/PeopleCell.h
@@ -0,0 +1,14 @@
+//
+//  PeopleCell.h
+//  Inneract
+//
+//  Created by Jim Liu on 3/8/15.
+//  Copyright (c) 2015 Emmanuel Texier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ParseUI.h>
+
+@interface PeopleCell : PFTableViewCell
+
+@end
---
+++ Inneract/PeopleCell.m
@@ -0,0 +1,23 @@
+//
+//  PeopleCell.m
+//  Inneract
+//
+//  Created by Jim Liu on 3/8/15.
+//  Copyright (c) 2015 Emmanuel Texier. All rights reserved.
+//
+
+#import "PeopleCell.h"
+
+@implementation PeopleCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
