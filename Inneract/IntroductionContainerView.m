be0448dfd0f08030a0b0a4f5244fa7c02cc86786
Emmanuel Texier <etexier@yahoo.com>
2015-03-13 09:01:17 -0700

    Fix issue where pages could go beyond last page. Disable vertical scrolling.

--- Inneract/IntroductionContainerView.m
+++ Inneract/IntroductionContainerView.m
@@ -13,13 +13,13 @@
 
 
 
-@interface IntroductionContainerView()
-@property (strong, nonatomic) IBOutlet UIView *contentView;
+@interface IntroductionContainerView ()
+@property(strong, nonatomic) IBOutlet UIView *contentView;
 
-@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property(strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property(weak, nonatomic) IBOutlet UIPageControl *pageControl;
 
-@property (nonatomic, strong) NSMutableArray *panels; //UIView
+@property(nonatomic, strong) NSMutableArray *panels; //UIView
 
 @end
 
@@ -28,75 +28,77 @@
     NSInteger _currentPanelIndex;
 }
 
--(id) initWithCoder:(NSCoder *) coder {
+- (id)initWithCoder:(NSCoder *)coder {
     self = [super initWithCoder:coder];
     if (self) {
         [self initSubViews];
     }
     return self;
-    
+
 }
 
--(id) initWithFrame:(CGRect)frame {
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         [self initSubViews];
     }
     return self;
-    
+
 }
 
--(id) init {
+- (id)init {
     self = [super init];
     if (self) {
         [self initSubViews];
     }
     return self;
-    
+
 }
 
--(void) initSubViews {
+- (void)initSubViews {
     UINib *nib = [UINib nibWithNibName:@"IntroductionContainerView" bundle:nil];
     [nib instantiateWithOwner:self options:nil];
-    
+
+    //self.scrollView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+    self.scrollView.delegate = self;
+    CGSize screenSize = [UIScreen mainScreen].bounds.size;
+    self.scrollView.contentSize =
+            CGSizeMake(screenSize.width * 3, // 3 screens only
+                    screenSize.height - 100); // should be less than frame height to prevent vertical scrolling
+
+
     FirstIntroductionView *first = [[FirstIntroductionView alloc] initWithFrame:[self frameByIndex:0]];
     SecondIntroductionView *second = [[SecondIntroductionView alloc] initWithFrame:[self frameByIndex:1]];
     ThirdIntroductionView *third = [[ThirdIntroductionView alloc] initWithFrame:[self frameByIndex:2]];
-    
+
     _panels = [NSMutableArray arrayWithCapacity:3];
     [_panels addObject:first];
     [_panels addObject:second];
     [_panels addObject:third];
-    
+
     [self.scrollView addSubview:first];
     [self.scrollView addSubview:second];
     [self.scrollView addSubview:third];
-    
+
     self.scrollView.pagingEnabled = YES;
     self.scrollView.showsHorizontalScrollIndicator = NO;
     self.scrollView.showsVerticalScrollIndicator = NO;
-    //self.scrollView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
-    self.scrollView.delegate = self;
-    self.scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width * 4, [UIScreen mainScreen].bounds.size.height);
-    //self.scrollView.frame = self.bounds;
-    
-    //self.scrollView.contentSize = CGSizeMake(0, self.scrollView.frame.size.height);
-    
+
     [self.contentView addSubview:self.scrollView];
-    
+
     //self.PageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, (self.scrollView.frame.origin.y + self.scrollView.frame.size.height - 60), self.frame.size.width, 36)];
     //[self.pageControl setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
     [self.pageControl addTarget:self action:@selector(showPanelAtPageControl) forControlEvents:UIControlEventValueChanged];
     self.pageControl.numberOfPages = self.panels.count;
     [self.contentView addSubview:self.pageControl];
-    
+
     self.contentView.frame = self.bounds;
     [self addSubview:self.contentView];
-    
+
     [self makePanelVisibleAtIndex:0];
 }
 
--(void)makePanelVisibleAtIndex:(NSInteger)panelIndex{
+- (void)makePanelVisibleAtIndex:(NSInteger)panelIndex {
 //    NSLog(@"Making %lu page visible", panelIndex);
 //    [UIView animateWithDuration:0.3 animations:^{
 //        for (int i = 0; i < self.panels.count; i++) {
@@ -111,24 +113,24 @@
 //    }];
 }
 
--(void)showPanelAtPageControl {
+- (void)showPanelAtPageControl {
     //Format and show new content
     //[self setContentScrollViewHeightForPanelIndex:self.CurrentPanelIndex animated:YES];
     [self makePanelVisibleAtIndex:self.pageControl.currentPage];
-    
+
     [self.scrollView setContentOffset:CGPointMake(self.pageControl.currentPage * [[UIScreen mainScreen] bounds].size.width, 0) animated:YES];
 }
 
-- (void)scrollViewDidScroll:(UIScrollView *)sender{
+- (void)scrollViewDidScroll:(UIScrollView *)sender {
     // Update the page when more than 50% of the previous/next page is visible
     CGFloat pageWidth = self.scrollView.frame.size.width;
     int page = floor((self.scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
-    if(page <= self.panels.count) {
+    if (page <= self.panels.count) {
         self.pageControl.currentPage = page;
     }
 }
 
--(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     //Update Page Control
     //Format and show new content
     [self makePanelVisibleAtIndex:self.pageControl.currentPage];
@@ -139,12 +141,5 @@
     return CGRectMake(mainScreenBounds.size.width * index, 0, mainScreenBounds.size.width, mainScreenBounds.size.height);
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
